2b1b962a178b956604e051bb6f0ef74265ab0639
David Zhu <dsz1@rice.edu>
2022-03-25 23:29:51 -0500

    Finishing the first part of Parstagram (Instagram) -- all basic user stories completed

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "Pap5fIdm0AkU7Jta3aw7mVi8Njg8qSoiWZvzXVzZ" // <- UPDATE
+                    $0.clientKey = "uqFm5V01D3RQixcQTxRSLa3vC3lqGKOKe4K3d8cb" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
